9154df3b21700cfb8d42994eb6e59fca98bc59fd
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-06-11 15:24:30 -0400

    [add] navbar stikcy top

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import Home from './views/home/Home';
-import Experience from './views/experience/Experience';
 import Description from './views/description/Description';
-import Skills from './views/skills/Skills';
 
 const App = () => {
     return (
         <>
             <Home />
             <Description />
-            <Experience />
-            <Skills />
         </>
     );
 };
--- src/views/description/Description.js
+++ src/views/description/Description.js
@@ -1,7 +1,17 @@
+import Navbar from '../../components/navbar/Navbar';
+import Experience from '../experience/Experience';
+import Skills from '../skills/Skills';
 import './Description.css';
 
 const Description = () => {
-    return <div id="description">Description Medium</div>;
+    return (
+        <div id="description">
+            <Navbar />
+            <div className="description-container">DESCIRPTION tal xual</div>
+            <Experience />
+            <Skills />
+        </div>
+    );
 };
 
 export default Description;
