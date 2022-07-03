d13c628768f950051585a8598e7ea9faab3fec83
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-07-03 11:57:02 -0400

    [mod] responsive description

--- src/views/description/Description.js
+++ src/views/description/Description.js
@@ -9,8 +9,8 @@ const Description = () => {
         <div id="description">
             <Navbar />
             <div className="description-container">
-                <div className="hstack p-5 stack-container">
-                    <div className="image-stack d-flex align-items-center justify-content-center">
+                <div className="row p-5">
+                    <div className="col col-md-6 col-12 image-stack d-flex align-items-center justify-content-center">
                         <p className="img-back-letter">M</p>
                         <img
                             src={portrait}
@@ -18,18 +18,20 @@ const Description = () => {
                             className="portrait-img-front animate__animated animate__zoomIn"
                         />
                     </div>
-                    <div className="align-items-center justify-content-center info-stack">
-                        <h1 className="mb-3">About Me</h1>
-                        <p>
-                            I am a passionate software developer with experience
-                            working on web services, web applications and mobile
-                            apps.
-                        </p>
-                        <p>
-                            Also, I'm interested in contribuing to open source
-                            projects from different developers and non-profit
-                            organizations.
-                        </p>
+                    <div className="col col-md-6 col-12 d-flex align-items-center justify-content-center info-stack">
+                        <div className="container">
+                            <h1 className="mb-3">About Me</h1>
+                            <p>
+                                I am a passionate software developer with
+                                experience working on web services, web
+                                applications and mobile apps.
+                            </p>
+                            <p>
+                                Also, I'm interested in contribuing to open
+                                source projects from different developers and
+                                non-profit organizations.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
