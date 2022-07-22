e3c37fa74135f39859c40a6b1b411f4a30681705
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-07-21 20:07:40 -0400

    [mod] project cards styles and info

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -2,23 +2,45 @@ import './ProjectCard.css';
 
 const ProjectCard = ({ project }) => {
     return (
-        <div className="card" style={{ width: '20rem' }}>
+        <div
+            className="card mt-5"
+            style={{
+                width: '25rem',
+                maxWidth: '25rem',
+                maxHeight: '40rem',
+                height: '40rem',
+            }}
+        >
             <img
                 src={project.img}
-                className="card-img-top p-3"
+                className="card-img-top p-4"
                 alt={project.title}
-                style={{ maxWidth: '15rem' }}
+                style={{
+                    maxWidth: '25rem',
+                    width: '25rem',
+                    maxHeight: '18rem',
+                    height: 'auto',
+                }}
             />
             <div className="card-body">
                 <h5 className="card-title">{project.title}</h5>
                 <p className="card-text">{project.description}</p>
             </div>
-            <a href={project.link} className="btn btn-proj-link m-3">
+            <a
+                href={project.link ? project.link : null}
+                className={`btn btn-proj-link m-3 ${
+                    !project.link && 'disabled-btn'
+                }`}
+                target="_blank"
+            >
                 Website
             </a>
             <a
-                href={project.sourceCode}
-                className="btn btn-sourcecode m-3 mt-0"
+                href={project.sourceCode ? project.sourceCode : null}
+                className={`btn btn-sourcecode m-3 mt-0 ${
+                    !project.sourceCode && 'disabled-btn'
+                }`}
+                target="_blank"
             >
                 Source Code
             </a>
