89f5b35163710a80d692b2a001468e3f29a61ba4
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-07-26 17:58:03 -0400

    [test] skills tests

--- src/views/skills/Skills.js
+++ src/views/skills/Skills.js
@@ -4,13 +4,14 @@ import './Skills.css';
 
 const Skills = () => {
     return (
-        <div id="skills" className="p-5">
-            <h1 className="text-center pt-3 pb-5 skills-title">Skills</h1>
+        <div id="skills" className="p-5" data-testid="skills">
+            <h1 className="text-center pt-3 pb-5 skills-title">SKILLS</h1>
             <div className="row d-flex align-items-center justify-content-center">
                 {skills.map((skill) => (
                     <div
                         className="col col-lg-4 col-md-8 col-sm-8 col-12"
                         key={skill.id}
+                        data-testid={skill.id}
                     >
                         <SkillCard skill={skill} />
                     </div>
