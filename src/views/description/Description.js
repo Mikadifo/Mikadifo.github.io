069f9316aeedf885e30e99fd5e666aee90bc85f5
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-08-26 23:42:10 -0400

    [mod] about me frame completed

--- src/views/description/Description.js
+++ src/views/description/Description.js
@@ -1,42 +1,30 @@
-import Experience from '../experience/Experience';
-import Skills from '../skills/Skills';
-import Projects from '../projects/Projects';
-import portrait from './../../img/face-img.png';
-import './Description.css';
 import {
     descriptionParagraph1,
     descriptionParagraph2,
 } from '../../constants/texts';
+import './Description.css';
 
 const Description = () => {
     return (
-        <>
-            <div id="description">
-                <div
-                    className="description-container"
-                    data-testid="description"
-                >
-                    <div className="row p-5">
-                        <div className="col col-lg-4 col-12 image-stack d-flex align-items-center justify-content-center">
-                            <p className="img-back-letter">M</p>
-                            <img
-                                src={portrait}
-                                alt="MIKADIFO"
-                                className="portrait-img-front animate__animated animate__zoomIn"
-                                data-testid="description-img"
-                            />
-                        </div>
-                        <div className="col col-lg-8 col-12 d-flex align-items-center justify-content-center info-stack">
-                            <div className="container about-container md-me-5">
-                                <h1 className="mb-3 about-title">ABOUT ME</h1>
-                                <p className="me-3">{descriptionParagraph1}</p>
-                                <p className="me-3">{descriptionParagraph2}</p>
-                            </div>
-                        </div>
-                    </div>
+        <div
+            className="row description-container h-100"
+            id="description"
+            data-testid="description"
+        >
+            <div className="col-4 my-auto text-center">
+                <h1 className="about-title">About Me</h1>
+            </div>
+            <div className="container col-2 justify-content-center d-flex">
+                <div className="about-separator" />
+            </div>
+            <div className="col-6 my-auto">
+                <div className="container about-container">
+                    <p className="me-3">{descriptionParagraph1}</p>
+                    <br />
+                    <p className="me-3">{descriptionParagraph2}</p>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
