0aeb23beffd000db716ed91c3f1abb2b1bf95977
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-08-27 15:18:44 -0400

    [mod] project card temp

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -2,36 +2,59 @@ import './ProjectCard.css';
 
 const ProjectCard = ({ project }) => {
     return (
-        <div className="card mt-5 card-project">
-            <img
-                src={project.img}
-                className="card-img-top p-4 card-img-project"
-                alt={project.title}
-            />
-            <div className="card-body">
-                <h5 className="card-title">{project.title}</h5>
-                <p className="card-text">{project.description}</p>
+        <div className="card-project">
+            <div className="row">
+                <div className="col-4 p-0">
+                    <img
+                        src={project.img}
+                        className="card-img-project"
+                        alt={project.title}
+                    />
+                </div>
+                <div className="col-8 project-description-container">
+                    <p className="card-text">
+                        <b>{project.title}: </b>
+                        {project.description}
+                    </p>
+                    <div className="row">
+                        <div className="col-6">
+                            <button>btn1</button>
+                        </div>
+                        <div className="col-6">
+                            <button>btn2</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <a
-                href={project.link ? project.link : null}
-                className={`btn btn-proj-link m-3 ${
-                    !project.link && 'disabled-btn'
-                }`}
-                target="_blank"
-            >
-                Website
-            </a>
-            <a
-                href={project.sourceCode ? project.sourceCode : null}
-                className={`btn btn-sourcecode m-3 mt-0 ${
-                    !project.sourceCode && 'disabled-btn'
-                }`}
-                target="_blank"
-            >
-                Source Code
-            </a>
         </div>
     );
 };
+
+//return (
+//<div className="card card-project">
+//<div className="card-body">
+//<h5 className="card-title">{project.title}</h5>
+//<p className="card-text">{project.description}</p>
+//</div>
+//<a
+//href={project.link ? project.link : null}
+//className={`btn btn-proj-link ${
+//!project.link && 'disabled-btn'
+//}`}
+//target="_blank"
+//>
+//Website
+//</a>
+//<a
+//href={project.sourceCode ? project.sourceCode : null}
+//className={`btn btn-sourcecode ${
+//!project.sourceCode && 'disabled-btn'
+//}`}
+//target="_blank"
+//>
+//Source Code
+//</a>
+//</div>
+//);
 
 export default ProjectCard;
--- src/views/projects/Projects.js
+++ src/views/projects/Projects.js
@@ -4,18 +4,13 @@ import './Projects.css';
 
 const Projects = () => {
     return (
-        <div id="projects" className="p-5" data-testid="projects">
-            <h1 className="text-center pt-3 pb-5 projects-title">PROJECTS</h1>
-            <div className="row align-items-center justify-content-center">
-                {projects.map((project) => (
-                    <div
-                        className="col col-lg-6 col-xl-4 col-md-6 col-12 d-flex align-items-center justify-content-center"
-                        key={project.id}
-                        data-testid={project.id}
-                    >
-                        <ProjectCard project={project} />
-                    </div>
-                ))}
+        <div id="projects" data-testid="projects">
+            <div className="project-bg justify-content-center d-flex">
+                <div className="project-container">
+                    <h1 className="text-center projects-title">My Work</h1>
+                    <ProjectCard project={projects[0]} />
+                    <div className="slider-controls">SOME Moving controls</div>
+                </div>
             </div>
         </div>
     );
