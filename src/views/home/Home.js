76d2ab678fb02e599001e5c88dd735fc9ec37411
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-08-28 11:48:07 -0400

    [mod] home sizing responsive

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -5,7 +5,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div id="home" className="row home-container" data-testid="home">
-            <div className="col-7 text-center">
+            <div className="col-xxl-7 col-lg-6 text-center ps-0 pe-0">
                 <div>
                     <div className="greeting-title">Hey there, I'm Michael</div>
                     <div className="rol-subtitle">
@@ -20,7 +20,7 @@ const Home = () => {
                     </a>
                 </div>
             </div>
-            <div className="col-5 my-auto text-center">
+            <div className="col-xxl-5 col-lg-6 order-lg-last order-first mb-lg-0 mb-5 align-items-center justify-content-center d-flex">
                 <img
                     src={portrait}
                     alt="MIKADIFO"
