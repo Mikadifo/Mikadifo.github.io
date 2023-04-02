234cd5dbd4f62a24aabb6de6c39c061ff3527598
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-01 23:00:49 -0400

    [add] added gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mikadifo`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
