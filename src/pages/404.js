4cd70b42e98b5ba5385960a8956512baba33ffb3
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-07 17:53:40 -0400

    [mod] added layout on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import Button from './../components/button';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 const NotFoundPage = () => {
     return (
-        <main className="text-center mt-16">
-            <h2 className="text-h2-sm lg:text-h2-md xl:text-h2-lg font-kufam mb-2">
-                Page not found
-            </h2>
-            <p className="text-p font-league-spartan mb-6">
-                Sorry 😔, we couldn’t find what you were looking for.
-            </p>
-            <Link to="/">
-                <Button>Go home</Button>
-            </Link>
-        </main>
+        <Layout>
+            <main className="text-center mt-16 text-blue-dark dark:text-white h-screen">
+                <h2 className="text-h2-sm lg:text-h2-md xl:text-h2-lg font-kufam mb-2">
+                    Page not found
+                </h2>
+                <p className="text-p font-league-spartan mb-6">
+                    Sorry 😔, we couldn’t find what you were looking for.
+                </p>
+                <Link to="/">
+                    <Button>Go home</Button>
+                </Link>
+            </main>
+        </Layout>
     );
 };
 
