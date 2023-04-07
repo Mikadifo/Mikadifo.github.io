1fc4a99064bea6555a5579ba8293494dadc25d01
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-07 17:48:36 -0400

    [add] added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,46 +1,19 @@
 import * as React from 'react';
+import Button from './../components/button';
 import { Link } from 'gatsby';
-
-const pageStyles = {
-    color: '#232129',
-    padding: '96px',
-    fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-    marginTop: 0,
-    marginBottom: 64,
-    maxWidth: 320,
-};
-
-const paragraphStyles = {
-    marginBottom: 48,
-};
-const codeStyles = {
-    color: '#8A6534',
-    padding: 4,
-    backgroundColor: '#FFF4DB',
-    fontSize: '1.25rem',
-    borderRadius: 4,
-};
 
 const NotFoundPage = () => {
     return (
-        <main style={pageStyles}>
-            <h1 style={headingStyles}>Page not found</h1>
-            <p style={paragraphStyles}>
+        <main className="text-center mt-16">
+            <h2 className="text-h2-sm lg:text-h2-md xl:text-h2-lg font-kufam mb-2">
+                Page not found
+            </h2>
+            <p className="text-p font-league-spartan mb-6">
                 Sorry 😔, we couldn’t find what you were looking for.
-                <br />
-                {process.env.NODE_ENV === 'development' ? (
-                    <>
-                        <br />
-                        Try creating a page in{' '}
-                        <code style={codeStyles}>src/pages/</code>.
-                        <br />
-                    </>
-                ) : null}
-                <br />
-                <Link to="/">Go home</Link>.
             </p>
+            <Link to="/">
+                <Button>Go home</Button>
+            </Link>
         </main>
     );
 };
