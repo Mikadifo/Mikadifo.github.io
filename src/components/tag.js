0ccb7c919d8d5ab310a44c03bf1b0518d0cce49d
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-08 22:08:06 -0400

    [mod] updated styles on experience

--- src/components/tag.js
+++ src/components/tag.js
@@ -3,13 +3,21 @@ import React from 'react';
 const Tag = ({ left, data }) => {
     const positionClasses = () => {
         if (left)
-            return 'rounded-r-lg pl-16 md:pl-32 xl:pl-64 pr-8 md:pr-16 lg:pr-20 bg-gradient-to-r md:mr-12 lg:mr-16';
-        return 'rounded-l-lg pr-16 md:pr-32 xl:pr-64 order-last pl-8 md:pl-16 lg:pl-20 bg-gradient-to-l md:ml-12 lg:ml-16';
+            return 'rounded-r-lg pl-16 md:pl-32 xl:pl-64 pr-8 md:pr-16 lg:pr-20 bg-gradient-to-r md:mr-12 lg:mr-16 md:hover:translate-x-6 hover:transition active:-translate-x-6';
+
+        return 'rounded-l-lg pr-16 md:pr-32 xl:pr-64 order-last pl-8 md:pl-16 lg:pl-20 bg-gradient-to-l md:ml-12 lg:ml-16 md:hover:-translate-x-6 hover:transition active:translate-x-6';
     };
 
     return (
-        <div className={`flex items-center ${left ? '' : 'justify-end'}`}>
-            <div
+        <div
+            className={`flex items-center duration-700 ${
+                left ? '' : 'justify-end'
+            }`}
+        >
+            <a
+                href={data.link}
+                target="_blank"
+                rel="noreferrer"
                 className={`from-blue-dark to-secondary py-4 md:py-8 lg:py-12 ${positionClasses()}`}
             >
                 <h4 className="font-kufam text-h4-sm lg:text-h4-md xl:text-h4-lg">
@@ -19,7 +27,7 @@ const Tag = ({ left, data }) => {
                 <p className="text-[12] md:text-[16] opacity-75 mt-3 font-league-spartan">
                     {data.date}
                 </p>
-            </div>
+            </a>
             <div
                 className={`hidden md:block ${
                     left ? 'md:mr-32 lg:mr-64' : 'md:ml-32 lg:ml-64'
