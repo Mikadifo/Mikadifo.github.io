6420a1d99620acf9e57d2f1d8bc0caa2d3b6eb5d
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-10 18:24:38 -0400

    [add] added contatct form, missing blobls

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import Button from './button';
+import Input from './input';
 
 const Contact = () => {
-    //TODO: Move inputs to compoenents maybe
     //TODO: Add blobs
     return (
         <div className="text-blue-dark dark:text-white mt-20 md:mt-24 lg:mt-48">
             <h3 className="text-h3-sm lg:text-h3-md xl:text-h3-lg font-bold font-kufam text-center mb-6 lg:mb-10">
                 Contact me
             </h3>
-            <div className="max-w-[19rem] md:max-w-[32rem] lg:max-w-[42rem]">
-                <form action="" className="grid in-w-[19rem]">
-                    <label htmlFor="name">Your name:</label>
-                    <input type="text" name="name" id="name" required />
-                    <label htmlFor="email">Your email:</label>
-                    <input type="email" name="email" id="email" required />
-                    <label htmlFor="idea">Your idea:</label>
-                    <textarea name="idea" id="" cols="30" rows="10" />
-                    <div className="mt-8 md:mt-12 lg:mt-14">
+            <div className="flex justify-center">
+                <form
+                    action=""
+                    className="grid gap-3 md:gap-4 lg:gap-8 min-w-[19rem] md:min-w-[32rem] lg:min-w-[42rem]"
+                >
+                    <Input label="Your name:" type="text" id="name" />
+                    <Input label="Your email:" type="email" id="email" />
+                    <Input label="Your idea:" type="textarea" id="idea" />
+                    <div className="text-center">
                         <Button>Let's talk</Button>
                     </div>
                 </form>
