b2fd3dd094c2fd1c73fe5a164455596fd1297dc4
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-11 14:39:21 -0400

    [mod] chenges styles

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -3,7 +3,7 @@ import { BsGithub } from 'react-icons/bs';
 
 const ProjectCard = ({ project }) => {
     return (
-        <div className="rounded-tr-lg rounded-bl-lg bg-gradient-to-br from-secondary via-blue-dark to-primary p-[2px]">
+        <div className="rounded-tr-lg rounded-bl-lg bg-gradient-to-br from-secondary via-blue-dark to-primary p-[2px] transition-all ease-in-out duration-300">
             <div className="flex flex-col justify-between rounded-tr-lg rounded-bl-lg p-4 lg:p-8 xl:p-12 bg-white dark:bg-blue-dark min-h-[350px]">
                 <div>
                     <h4 className="text-h4-sm lg:text-h4-md xl:text-h4-lg font-kufam mb-2 lg:mb-4 xl:mb-6">
