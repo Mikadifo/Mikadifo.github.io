272429ddc7ee3d4b8abcbf6e16066bc54827f323
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-14 09:55:56 -0400

    [mod] updated default email

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,8 @@ import * as React from 'react';
 const Footer = () => {
     return (
         <div className="text-blue-dark dark:text-white text-p font-league-spartan text-center mt-16 md:mt-20 lg:mt-28">
-            <a
-                href="mailto:mikadifopadillaheredia@gmail.com"
-                className="hover:underline"
-            >
-                mikadifopadillaheredia@gmail.com
+            <a href="mailto:contact@mikadifo.com" className="hover:underline">
+                contact@mikadifo.com
             </a>
             <p className="opacity-75 py-3 lg:py-4 border-t-[1px] border-blue-dark/25 dark:border-white/25 mt-2 lg:3">
                 © 2022 Mikadifo
