4d3c0b643316590c4a59201b098fb93324001860
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-05-04 10:42:38 -0400

    [test] added projects testing

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     transform: {
-        '\\.svg': `<rootDir>/__mock__/svgTransform.js`,
+        '\\.svg': `<rootDir>/__mocks__/svgTransform.js`,
         '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
     },
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-        '\\.svg': `<rootDir>/__mock__/svgTransform.js`,
-        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mock__/file-mocks.js`,
+        '\\.svg': `<rootDir>/__mocks__/svgTransform.js`,
+        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mocks.js`,
     },
     testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
     transformIgnorePatterns: [
