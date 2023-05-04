e8dbd23f78191c61f543fbe0d6804060413c20a7
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-05-03 21:27:07 -0400

    [fix] transforming svg for test

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     transform: {
+        '\\.svg': `<rootDir>/__mock__/svgTransform.js`,
         '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
     },
     moduleNameMapper: {
         '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+        '\\.svg': `<rootDir>/__mock__/svgTransform.js`,
+        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mock__/file-mocks.js`,
     },
     testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
     transformIgnorePatterns: [
