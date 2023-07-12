4fe6627f0d0aa3730fcaab440c9f69241db608c5
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-07-12 13:35:08 -0400

    add: local fonts instead of using google font link

---
+++ gatsby-ssr.js
@@ -0,0 +1,22 @@
+//import * as React from 'react';
+
+//export const onRenderBody = ({ setHeadComponents }) => {
+//setHeadComponents([
+//<link
+//rel="preload"
+//href="./src/fonts/Kufam.ttf"
+//as="font"
+//type="font/ttf"
+//crossOrigin="anonymous"
+//key="kufamFont"
+///>,
+//<link
+//rel="preload"
+//href="./src/fonts/LeagueSpartan.ttf"
+//as="font"
+//type="font/ttf"
+//crossOrigin="anonymous"
+//key="LeagueSpartanFont"
+///>,
+//]);
+//};
