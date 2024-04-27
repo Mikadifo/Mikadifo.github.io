d70c10775a2f6b953a232a8522268ac87b6fbf2b
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-27 16:38:19 -0400

    fix: updated tests

--- src/components/__tests__/about.js
+++ src/components/__tests__/about.js
@@ -12,7 +12,7 @@ describe('About', () => {
         expect(about).toBeInTheDocument();
         expect(about).toHaveTextContent('About me');
         expect(about).toHaveTextContent(
-            'As an experienced front-end software developer and freelancer, my journey spans over 3 years in web development. Driven by curiosity, I thrive on exploring and mastering emerging technologies.',
+            'As an experienced full stack software developer and freelancer, my journey spans over 4 years in web development. Driven by curiosity, I thrive on exploring and mastering emerging technologies.',
         );
         expect(about).toHaveTextContent(
             'My dedication to delivering innovative solutions fuels my passion for building extraordinary software. Always seeking new challenges and knowledge to stay ahead in this fast growing industry.',
--- src/components/__tests__/callToAction.js
+++ src/components/__tests__/callToAction.js
@@ -10,8 +10,8 @@ describe('Call To Action', () => {
     it('renders correctly', () => {
         const callToAction = render(<CallToAction />).container;
         expect(callToAction).toBeInTheDocument();
-        expect(callToAction).toHaveTextContent("Hey, I'm Mikadifo");
-        expect(callToAction).toHaveTextContent('Front End Developer');
+        expect(callToAction).toHaveTextContent("Hey, I'm Michael");
+        expect(callToAction).toHaveTextContent('Web Developer');
         const button = callToAction.getElementsByTagName('a')[0];
         expect(button).toBeInTheDocument();
         expect(button).toHaveTextContent('Contact me');
