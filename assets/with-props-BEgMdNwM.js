ea66a58e04ba5b42c051ef3cf33062a9a000dcbe
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-17 00:02:06 -0400

    Updates

--- assets/with-props-BEgMdNwM.js
+++ assets/with-props-BEgMdNwM.js
@@ -1,1 +1,21 @@
-import{s as p,t,v as e,w as o,a as s,x as u}from"./chunk-D4RADZKF-Br10xN1u.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
+import {
+  s as p,
+  t,
+  v as e,
+  w as o,
+  a as s,
+  x as u,
+} from "./chunk-D4RADZKF-Br10xN1u.js";
+function i(a) {
+  return function () {
+    const r = { params: o(), loaderData: e(), actionData: t(), matches: p() };
+    return s.createElement(a, r);
+  };
+}
+function m(a) {
+  return function () {
+    const r = { params: o(), loaderData: e(), actionData: t(), error: u() };
+    return s.createElement(a, r);
+  };
+}
+export { m as a, i as w };
