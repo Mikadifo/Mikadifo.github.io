3186f545e8c0a9a2d8af1ece39548d075b689450
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-17 00:06:40 -0400

    mod: ssr disabled

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/Mikadifo.github.io/",
+  base: "/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
 });
