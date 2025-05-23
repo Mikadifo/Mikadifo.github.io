092fc79b04d8ecfd3ce990d232072ac2d257d171
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-23 18:54:03 -0400

    fix: footer responsive full

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="text-dark opacity-50 text-center py-2 lg:py-4 border-t border-[rgba(6,4,45,0.12)] font-normal text-lg lg:text-xl">
+    <div className="text-dark opacity-50 text-center py-2 lg:py-4 border-t border-[rgba(6,4,45,0.12)] font-normal text-base sm:text-lg lg:text-xl">
       © {new Date().getFullYear()} Mikadifo
     </div>
   );
