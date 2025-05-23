cc9f44ebc1628b0f00b15f293e52f98d654c01a1
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-23 19:33:21 -0400

    fix: experience responsive ful

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,4 +1,4 @@
-import { getYearsAndMonths } from "~/utils/date";
+import { getYearsAndMonths, getShortFormat } from "~/utils/date";
 import CalendarIcon from "./../assets/icons/calendarIcon.svg?react";
 
 type ExperienceProps = {
@@ -17,7 +17,7 @@ export interface ExperienceCard {
 
 export default function Experience({ title, data }: ExperienceProps) {
   return (
-    <div className="bg-dark py-32 lg:py-48 px-8 lg:px-0">
+    <div className="bg-dark py-20 sm:py-32 lg:py-48 px-4 sm:px-8 lg:px-0">
       <div className="lg:w-[928px] mx-auto">
         <h3 className="text-4xl lg:text-5xl font-bold font-kufam text-white mb-8 lg:mb-16">
           {title}
@@ -27,7 +27,7 @@ export default function Experience({ title, data }: ExperienceProps) {
           {data.map((item) => (
             <div
               key={item.id}
-              className="flex justify-between bg-light-05 px-6 py-6 rounded-xl"
+              className="flex justify-between bg-light-05 px-5 py-5 sm:px-6 sm:py-6 rounded-xl flex-col sm:flex-row"
             >
               <div className="flex">
                 <img
@@ -35,6 +35,7 @@ export default function Experience({ title, data }: ExperienceProps) {
                   alt={item.heading + " company logo"}
                   className="rounded-xl me-3 lg:me-4 w-14 h-14 lg:w-[72px] lg:h-[72px]"
                 />
+
                 <div className="flex flex-col lg:gap-2">
                   <span className="font-bold text-2xl lg:text-[32px]">
                     {item.heading}
@@ -45,14 +46,14 @@ export default function Experience({ title, data }: ExperienceProps) {
                 </div>
               </div>
 
-              <div className="flex gap-2 lg:gap-3 w-[177px] lg:w-[212px]">
-                <CalendarIcon className="mt-[1.4px] lg:mt-1.5 w-5 lg:w-auto" />
+              <div className="flex gap-2 lg:gap-3 sm:w-[177px] lg:w-[212px] mt-4 sm:mt-0 items-center sm:items-start">
+                <CalendarIcon className="sm:mt-[1.4px] lg:mt-1.5 w-5 lg:w-auto" />
 
-                <div className="flex flex-col text-base lg:text-xl font-normal">
+                <div className="flex flex-row sm:flex-col text-base lg:text-xl font-normal justify-between sm:justify-start w-full sm:w-auto">
                   <span>
-                    {item.start} - {item.end}
+                    {getShortFormat(item.start)} - {getShortFormat(item.end)}
                   </span>
-                  <span className="opacity-50 mt-[-4px]">
+                  <span className="opacity-50 sm:mt-[-4px]">
                     {getYearsAndMonths(item.start, item.end)}
                   </span>
                 </div>
