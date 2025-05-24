9d5c7e105c7cc98f9a73a9831bda9c231b681e6b
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-24 01:13:01 -0400

    add: floating illustration

--- app/caseStudies/UnderConstruction.tsx
+++ app/caseStudies/UnderConstruction.tsx
@@ -1,9 +1,9 @@
-import CreativeIllustration from "./../assets/imgs/creativeIllustration.svg?react";
+import AnimatedCreativeIllustration from "~/components/AnimatedCreativeIllustration";
 
 export default function UnderConstruction() {
   return (
     <div className="flex flex-col sm:flex-row items-center gap-12 lg:gap-16">
-      <CreativeIllustration className="w-2/3 sm:w-1/2 lg:w-2/3 h-auto" />
+      <AnimatedCreativeIllustration className="w-2/3 sm:w-1/2 lg:w-2/3 h-auto" />
 
       <div className="text-dark w-full sm:w-1/2 lg:w-1/3 text-center sm:text-left">
         <h2 className="font-kufam text-3xl sm:text-4xl lg:text-5xl font-bold sm:mb-3 md:mb-4 lg:mb-5">
