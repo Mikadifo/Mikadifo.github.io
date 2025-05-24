e0aff1153946cb565bb8bd004f535735761a4909
Mikadifo <Mikadifo@users.noreply.github.com>
2025-05-24 19:19:05 +0000

    deploy: d9aaccf89180f4542fff51871bf021555432b4fe

--- assets/with-props-Ca4YHJ5N.js
+++
@@ -1,1 +0,0 @@
-import{v as p,w as t,x as e,s as o,a as s,y as u}from"./chunk-D4RADZKF-Bw9RnjSB.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
