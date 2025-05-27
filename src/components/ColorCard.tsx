f785f4adae8b2ed44c9a3f6a90897d183306eb21
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-26 23:57:48 -0400

    add: gradient picker

--- src/components/ColorCard.tsx
+++ src/components/ColorCard.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import PickerIcon from "./../assets/icons/pickerIcon.svg?react";
 
 type ColorCardProps = {
@@ -16,7 +16,11 @@ export default function ColorCard({
   bordered = false,
   gradient = [],
 }: ColorCardProps) {
+  const [moving, setMoving] = useState<boolean>(true);
+  const [gradientHex, setGradientHex] = useState<string | null>(null);
   const pickerRef = useRef<SVGSVGElement | null>(null);
+
+  const gradientColors = gradient.map((color) => color.split(" ")[0]).reverse();
   let background = {};
 
   if (gradient.length > 0) {
@@ -28,17 +32,26 @@ export default function ColorCard({
   }
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!moving) return;
+
     const colorBox = event.currentTarget.getBoundingClientRect();
     const x = Math.abs(event.clientX - colorBox.right);
     const y = Math.abs(event.clientY - colorBox.top);
+
+    const colorDivisions = (colorBox.right - colorBox.left) / gradient.length;
+    const colorIndex = Math.floor(x / colorDivisions);
+    setGradientHex(gradientColors[colorIndex]);
 
     pickerRef.current!.style.right = x + "px";
     pickerRef.current!.style.top = y + "px";
   };
 
   const handleMouseExit = () => {
+    if (!moving) return;
+
     pickerRef.current!.style.right = "12px";
     pickerRef.current!.style.top = "12px";
+    setGradientHex(null);
   };
 
   return (
@@ -46,6 +59,7 @@ export default function ColorCard({
       <div
         onMouseMove={gradient.length > 0 ? handleMouseMove : undefined}
         onMouseLeave={gradient.length > 0 ? handleMouseExit : undefined}
+        onClick={gradient.length > 0 ? () => setMoving(!moving) : undefined}
         className={`relative size-[153.6px] font-normal text-2xl p-3 flex items-end justify-center rounded-xl ${labelDark ? "text-dark" : "text-white"} ${bordered ? "border-1 border-dark-25" : ""} ${gradient.length > 0 ? "cursor-none" : ""}`}
         style={{ ...background }}
       >
@@ -58,7 +72,7 @@ export default function ColorCard({
       </div>
 
       <div className="text-xl font-normal py-2 px-5 w-full bg-dark-04 text-dark-75 rounded-xl mt-2 flex justify-center">
-        {hex}
+        {gradient.length > 0 ? gradientHex || "Pick Color" : hex}
       </div>
     </div>
   );
