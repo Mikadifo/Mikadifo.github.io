185b4b90002754b8970214b77655bc39438783cb
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-26 22:19:05 -0400

    fix: tag sizing

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -8,7 +8,7 @@ type TagProps = {
 export default function Tag({ label, img }: TagProps) {
   return (
     <div className="flex gap-2 py-1 px-5 md:py-2 md:px-6 bg-dark text-white text-lg md:text-2xl font-normal rounded-full items-center">
-      {img ? React.createElement(img, { className: "size-5 md:size-6" }) : ""}
+      {img ? React.createElement(img, { className: "size-5 lg:size-6" }) : ""}
       {label}
     </div>
   );
