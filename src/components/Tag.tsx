f54b472c66ddfe8b092236369ba6d2eb435acf30
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-05-27 15:06:24 -0400

    mod: added link to tags

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -3,13 +3,19 @@ import React from "react";
 type TagProps = {
   label: string;
   img?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  href: string;
 };
 
-export default function Tag({ label, img }: TagProps) {
+export default function Tag({ label, img, href }: TagProps) {
   return (
-    <div className="flex gap-2 py-1 px-5 md:py-2 md:px-6 bg-dark text-white text-lg md:text-2xl font-normal rounded-full items-center">
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex gap-2 py-1 px-5 md:py-2 md:px-6 bg-dark text-white text-lg md:text-2xl font-normal rounded-full items-center hover:opacity-80"
+    >
       {img ? React.createElement(img, { className: "size-5 lg:size-6" }) : ""}
       {label}
-    </div>
+    </a>
   );
 }
